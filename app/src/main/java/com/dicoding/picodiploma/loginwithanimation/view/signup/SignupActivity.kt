0bcca9c2029d7bdbaab1d0411a604a820db82625
Kevin Alfito <kevinalfito69@gmail.com>
2023-04-17 20:36:41 +0700

    Initial Commit

---
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/signup/SignupActivity.kt
@@ -0,0 +1,123 @@
+package com.dicoding.picodiploma.loginwithanimation.view.signup
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.picodiploma.loginwithanimation.databinding.ActivitySignupBinding
+import com.dicoding.picodiploma.loginwithanimation.model.UserModel
+import com.dicoding.picodiploma.loginwithanimation.model.UserPreference
+import com.dicoding.picodiploma.loginwithanimation.view.ViewModelFactory
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+class SignupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var signupViewModel: SignupViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+        setupViewModel()
+        setupAction()
+        showAnimation()
+    }
+
+    private fun showAnimation() {
+        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatMode = ObjectAnimator.REVERSE
+            repeatCount = ObjectAnimator.INFINITE
+        }.start()
+        val title = alphaAnimator(binding.titleTextView)
+        val name = alphaAnimator(binding.nameTextView)
+        val nameInput = alphaAnimator(binding.nameEditTextLayout)
+        val email = alphaAnimator(binding.emailTextView)
+        val emailInput = alphaAnimator(binding.emailEditTextLayout)
+        val password = alphaAnimator(binding.passwordTextView)
+        val passwordInput = alphaAnimator(binding.passwordEditTextLayout)
+        val btnSignup = alphaAnimator(binding.signupButton)
+
+        AnimatorSet().apply {
+            playSequentially(
+                title,
+                name,
+                nameInput,
+                email,
+                emailInput,
+                password,
+                passwordInput,
+                btnSignup
+            )
+            start()
+        }
+    }
+
+    private fun alphaAnimator(view: View): ObjectAnimator {
+        return ObjectAnimator.ofFloat(view, View.ALPHA, 1f).setDuration(500)
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupViewModel() {
+        signupViewModel = ViewModelProvider(
+            this,
+            ViewModelFactory(UserPreference.getInstance(dataStore))
+        )[SignupViewModel::class.java]
+    }
+
+    private fun setupAction() {
+        binding.signupButton.setOnClickListener {
+            val name = binding.nameEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            when {
+                name.isEmpty() -> {
+                    binding.nameEditTextLayout.error = "Masukkan email"
+                }
+                email.isEmpty() -> {
+                    binding.emailEditTextLayout.error = "Masukkan email"
+                }
+                password.isEmpty() -> {
+                    binding.passwordEditTextLayout.error = "Masukkan password"
+                }
+                else -> {
+                    signupViewModel.saveUser(UserModel(name, email, password, false))
+                    AlertDialog.Builder(this).apply {
+                        setTitle("Yeah!")
+                        setMessage("Akunnya sudah jadi nih. Yuk, login dan belajar coding.")
+                        setPositiveButton("Lanjut") { _, _ ->
+                            finish()
+                        }
+                        create()
+                        show()
+                    }
+                }
+            }
+        }
+    }
+}
